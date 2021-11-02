835907d78982f1a18055b338689b65035db8bbb1
stenkristel <stenkristel2016@gmail.com>
2021-11-02 11:48:24 +0100

    Epiclist made

---
+++ PYTB1L6CanYouHandleTheList/PYTB1L6CanYouHandleTheList.py
@@ -0,0 +1,12 @@
+epiclist = [5, "Melon", "BeeMovie", ":D", "What is this list?"]
+print(epiclist)
+
+epiclist.append("4/6")
+print(epiclist)
+epiclist.pop(5)
+
+epiclist.reverse()
+print(epiclist)
+
+epiclist.insert(3, "PartyPooper")
+print(epiclist)
